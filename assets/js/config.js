bba9453e4fd83f16150ba4a83813a155a0916ad8
fullstackprocrastinator <111703058+fullstackprocrastinator@users.noreply.github.com>
2025-11-07 21:38:34 +0000

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   DATA_URL: 'assets/data/salaries.json',
   PENDING_URL: 'assets/data/pending.json',
-  FORMSPREE_ENDPOINT: 'https://formspree.io/f/YOUR_FORM_ID', // CHANGE THIS
+  FORMSPREE_ENDPOINT: 'https://formspree.io/f/xnnlaqka',
   COUNTRIES: ['United Kingdom', 'United States', 'Australia', 'Canada', 'Ireland', 'UAE', 'Singapore', 'Germany', 'France', 'South Africa', 'Other'],
   UK_COUNTIES: ['London', 'Manchester', 'Birmingham', 'Leeds', 'Glasgow', 'Bristol', 'Cardiff', 'Edinburgh', 'Liverpool', 'Sheffield'],
   USA_STATES: ['California', 'Texas', 'New York', 'Florida', 'Illinois', 'Pennsylvania', 'Ohio', 'Georgia', 'North Carolina', 'Michigan'],
