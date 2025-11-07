f202eb95fd723021be95b049576628b7efb6f007
fullstackprocrastinator <111703058+fullstackprocrastinator@users.noreply.github.com>
2025-11-07 22:16:05 +0000

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,9 +2,34 @@ const CONFIG = {
   DATA_URL: 'assets/data/salaries.json',
   PENDING_URL: 'assets/data/pending.json',
   FORMSPREE_ENDPOINT: 'https://formspree.io/f/YOUR_FORM_ID', // CHANGE THIS
-  COUNTRIES: ['United Kingdom', 'United States', 'Australia', 'Canada', 'Ireland', 'UAE', 'Singapore', 'Germany', 'France', 'South Africa', 'Other'],
-  UK_COUNTIES: ['London', 'Manchester', 'Birmingham', 'Leeds', 'Glasgow', 'Bristol', 'Cardiff', 'Edinburgh', 'Liverpool', 'Sheffield'],
-  USA_STATES: ['California', 'Texas', 'New York', 'Florida', 'Illinois', 'Pennsylvania', 'Ohio', 'Georgia', 'North Carolina', 'Michigan'],
+  COUNTRIES: [
+    'United Kingdom', 'United States', 'Australia', 'Canada', 'Ireland',
+    'UAE', 'Singapore', 'Germany', 'France', 'South Africa', 'Other'
+  ],
+  UK_COUNTIES: [
+    'Bedfordshire', 'Berkshire', 'Bristol', 'Buckinghamshire', 'Cambridgeshire',
+    'Cheshire', 'City of London', 'Cornwall', 'Cumbria', 'Derbyshire',
+    'Devon', 'Dorset', 'Durham', 'East Riding of Yorkshire', 'East Sussex',
+    'Essex', 'Gloucestershire', 'Greater London', 'Greater Manchester',
+    'Hampshire', 'Herefordshire', 'Hertfordshire', 'Isle of Wight', 'Kent',
+    'Lancashire', 'Leicestershire', 'Lincolnshire', 'Merseyside', 'Norfolk',
+    'North Yorkshire', 'Northamptonshire', 'Northumberland', 'Nottinghamshire',
+    'Oxfordshire', 'Rutland', 'Shropshire', 'Somerset', 'South Yorkshire',
+    'Staffordshire', 'Suffolk', 'Surrey', 'Tyne and Wear', 'Warwickshire',
+    'West Midlands', 'West Sussex', 'West Yorkshire', 'Wiltshire', 'Worcestershire'
+  ],
+  USA_STATES: [
+    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
+    'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho',
+    'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada',
+    'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+    'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',
+    'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
+    'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
+    'West Virginia', 'Wisconsin', 'Wyoming'
+  ],
   CHART_COLORS: {
     light: ['#1a3b5d', '#2c5f8a', '#3d82b7', '#5aa0d4', '#77b8e6'],
     dark: ['#4a7bb7', '#6a9bd5', '#8ab9e6', '#a8d0f8', '#c6e4ff']
